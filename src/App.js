b86d7450a220eda7e7604d510561d95e8f793916
aproskur <ao.panova@gmail.com>
2023-04-04 13:58:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import game from './data/_game.json'
+import GameScreen from './components/GameScreen'
+import { useCallback } from 'react';
+import { useState } from 'react';
+
 
 function App() {
+
+ 
+  //Приложение будет получать от сервера только screen, но мне сейчас нужен таймлайн, 
+  //чтобы отображать экраны последовательно, пока нет API
+  const timeline = game.timeline;
+  const screenId = timeline[0];  //временно захардкожен первый элемент массива таймлайн
+
+  //Это захардкоженный второй скрин gameScreen2 из _game.json, чтобы быстрее посмотреть, как всё работает
+  const newTest = game.gameScreens[timeline[1]];
+
+
+  //  стейт. обновляет currentScreen. CurrentScreen передаётся в компонент GameScreen  
+  // когда будет api, useState в значение по умолчанию вместо game.gameScreens[timeline[0]] будет передаваться экран, полученый от api
+  const [currentScreen, setScreen] = useState(game.gameScreens[timeline[0]]);
+
+
+  //функция, имя которой передаётся в GameScreen
+  //Должна запрашивать следующий экран и обновлять стейт приложения, чтобы передать новый экран в GameScreen
+  //Сейчас захордкожен следующий экран  game.gameScreens[timeline[1]];. когда будет api, внутри этой фунции 
+  //будет делаться запрос нового экрана (GET).
+  // стейт обновляется, и в GameScreen уходит новый экран
+    const handleSetScreen = useCallback(() => {
+      const newScreen =  newTest;
+      setScreen(newScreen);
+    }, ); 
+
+  //в GameScreen передаю экран currentScreen и callback handleSetScreen
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GameScreen function = { handleSetScreen } screen = { currentScreen }/>
+    </>
   );
 }
 
